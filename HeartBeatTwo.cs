10457f75d0d021ab182b6cdf5fe5a535c1790801
Christopher Brown <riv4r@live.com>
2019-10-11 00:05:57 -0400

    Program commit

---
+++ HeartBeatTwo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+using System.ServiceProcess;
+
+namespace NetCoreTestService
+{
+    public class HeartbeatTwo : ServiceBase
+    {
+        private readonly Timer _timer;
+
+        public HeartbeatTwo()
+        {
+            ServiceName = "HeartBeatTwo";
+
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(@"C:\_test\HeartBeat.txt", lines); //todo: put this into config file
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Start(args);
+        }
+
+        protected override void OnStop()
+        {
+            Stop();
+        }
+
+        protected override void OnPause()
+        {
+            Pause();
+        }
+
+        public void Start(string[] args)
+        {
+            _timer.Start();
+        }
+
+        public new void Stop()
+        {
+            _timer.Stop();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ServiceProcess;
+using Topshelf;
+
+
+namespace NetCoreTestService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Hello World
+
+            Console.WriteLine("Hello World");
+
+            #endregion
+
+            #region Topshelf
+            //Topshelf dependancy             
+            //var exitCode = HostFactory.Run(x =>
+            //{
+            //    x.Service<Heartbeat>(s =>
+            //    {
+            //        s.ConstructUsing(heartbeat => new Heartbeat());
+            //        s.WhenStarted(heartbeat => heartbeat.Start());
+            //        s.WhenStopped(heartbeat => heartbeat.Stop());
+            //    });
+
+            //    x.RunAsLocalSystem();
+            //    x.SetServiceName("HeartbeatService");
+            //    x.SetDisplayName("Heartbeat Service");
+            //    x.SetDescription("Totally not a key logger.");
+
+            //});
+
+            //int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            //Environment.ExitCode = exitCodeValue;            
+            #endregion
+
+            #region .net Core Webhost                        
+            HeartbeatTwo service = new HeartbeatTwo();
+
+            if (!Environment.UserInteractive)
+            {
+                ServiceBase.Run(service);
+            }
+            else
+            {
+                service.Start(args);
+
+                Console.WriteLine("Press 'P' to pause or any other key to stop...");
+                Console.ReadKey(true);
+
+                //IO exception, need to find way to release file or alter what is being written to file
+                //var keyPress = Console.ReadKey(true);
+                //if (keyPress.Key == ConsoleKey.P)
+                //{
+                //    service.Pause();
+                //}
+                //else
+                //{
+                //    service.Stop();
+                //}
+
+                service.Stop();
+                                    
+            }
+            #endregion
+        }
+    }
+}
